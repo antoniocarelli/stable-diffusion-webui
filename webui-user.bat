a64138d39b920dc318543e769ba0b18d1b33f072
Antonio Carelli <antonio@americocarelli.com.br>
2026-07-22 13:02:15 -0300

    chore: configure local python path, api arguments, and custom repository URLs in webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,12 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\anton\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--api --listen --enable-insecure-extension-access
+
+set STABLE_DIFFUSION_REPO=https://github.com/w-e-w/stablediffusion.git
+set STABLE_DIFFUSION_XL_REPO=https://github.com/w-e-w/generative-models.git
 
 call webui.bat
+
